d2329bfe5f60f0c50530d65fabfbdb20c8f9e3c5
CyberFlaw <kannan.pappus@gmail.com>
2022-02-12 02:37:19 +0530

    Styled Landing Page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
 
 import Link from "next/link";
-import styles from "../styles/Home.module.css";
+import Image from "next/image";
+
+import readImage from "../assets/read.png";
+import writeImage from "../assets/write.png";
 
 export default function Home() {
   useEffect(() => {
@@ -9,23 +12,79 @@ export default function Home() {
   }, []);
 
   return (
-    <div
-      className={styles.container}
-      style={{ display: "flex", flexDirection: "column" }}
-    >
-      <h1><p class="text-blue-600">hello gois</p></h1>
-      <ul>
-        <li>
-          <Link href="/submit-pickup">
-            <a>Pickup someone</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/read-pickup">
-            <a>Fall for someone</a>
-          </Link>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className="flex flex-col bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500 h-auto sm:h-screen justify-start items-center">
+        <h1 className="text-7xl sm:mt-24 mt-16 text-white">Valentine</h1>
+
+        <div className="w-full flex sm:flex-row flex-col justify-center items-center mt-0 sm:mt-3 sm:mb-20">
+          {/* Submit pickup */}
+          <div id="submit-pickup">
+            <Link href="/read-pickup" passHref>
+              <button className=" px-8 py-2 mt-8 rounded-3xl text-gray-100 font-semibold uppercase tracking-wide">
+                <div className="bg-black hover:opacity-60 opacity-0 h-full">
+                  {" "}
+                </div>
+                <div className="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs">
+                  <div className="-m-10">
+                    <Image
+                      src={writeImage}
+                      alt="write img"
+                      height={235}
+                      width={235}
+                    />
+                    <h3 className="text-black sm:text-xs text-sm mx-5 mb-5">
+                      Guys! This is your chance. Write the best pickup lines you
+                      possibly can. If you crack her up, you might be able to
+                      snatch her heart.
+                    </h3>
+                    <h2 className="text-black sm:text-xs text-sm mx-5 mb-5">
+                      {" "}
+                      Best of luck, Valiant Hearts!
+                    </h2>
+                  </div>
+                </div>
+              </button>
+            </Link>
+          </div>
+          {/* Read pickup */}
+          <div id="read-pickup" className="sm:mb-0 mb-10">
+            <Link href="/read-pickup" passHref>
+              <button className=" px-8 py-2 mt-8 rounded-3xl text-gray-100 font-semibold uppercase tracking-wide">
+                <div className="bg-black hover:opacity-60 opacity-0 h-full">
+                  {" "}
+                </div>
+                <div className="bg-white font-semibold text-center rounded-3xl border shadow-lg p-10 max-w-xs">
+                  <div className="-m-10">
+                    <Image
+                      src={readImage}
+                      alt="read img"
+                      height={200}
+                      width={200}
+                    />
+                    <h3 className="text-black sm:text-xs text-sm mx-5 mb-5">
+                      Gals! I’ve heard that there are a bunch of Gentlemen on
+                      the other side of this door with their finest jokes. If
+                      you are flattered do let them know, they would love your
+                      kind gesture. Remember you only get one pick.
+                    </h3>
+                    <h2 className="text-black sm:text-xs text-sm mx-5 mb-5">
+                      {" "}
+                      Choose wisely, Alluring Maiden
+                    </h2>
+                  </div>
+                </div>
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-1 flex-col justify-center items-center ">
+          <h3 className="text-white text-xs">Brought to you by</h3>
+          <h2 className="text-white text-xm">
+            {" "}
+            The Department of Computer Science
+          </h2>
+        </div>
+      </div>
+    </>
   );
 }
